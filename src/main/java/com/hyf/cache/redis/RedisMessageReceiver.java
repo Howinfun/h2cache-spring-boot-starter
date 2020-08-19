69faa93421980e55f0aa593499de46510b1914d6
111150 <chunhui.wang@aihuishou.com>
2020-08-19 14:22:51 +0800

    添加日志

--- src/main/java/com/hyf/cache/redis/RedisMessageReceiver.java
+++ src/main/java/com/hyf/cache/redis/RedisMessageReceiver.java
@@ -1,19 +1,21 @@
 package com.hyf.cache.redis;
 import com.alibaba.fastjson.JSONObject;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.Cache;
 import org.springframework.cache.ehcache.EhCacheCacheManager;
 
 @Data
+@Slf4j
 public class RedisMessageReceiver {
 
     private EhCacheCacheManager ehCacheCacheManager;
 
     public void cacheEvictMessage(String message) {
-        System.out.println("redis订阅者接受消息:{"+message+"},更新缓存");
+        log.info("redis订阅者接受消息:{},清除缓存",message);
         JSONObject json = JSONObject.parseObject(message);
         String name = json.getString("name");
-        if(name != null && "".equals(name)){
+        if(name != null && !"".equals(name)){
             Cache ehCache = ehCacheCacheManager.getCache(name);
             if (null != ehCache) {
                 Object key = json.get("name");
